9b3e0504d4046abcd40253b170a6bf74bf1107ba
Garvit Goyal <garvitgoyal2k6@gmail.com>
2026-03-15 18:56:16 +0530

    adding new file

---
+++ Assembly_endgame/src/languages/language.js
@@ -0,0 +1,46 @@
+export const languages = [
+    {
+        name: "HTML",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA"
+    },
+    {
+        name: "CSS",
+        backgroundColor: "#328AF1",
+        color: "#F9F4DA"
+    },
+    {
+        name: "JavaScript",
+        backgroundColor: "#F4EB13",
+        color: "#1E1E1E"
+    },
+    {
+        name: "React",
+        backgroundColor: "#2ED3E9",
+        color: "#1E1E1E"
+    },
+    {
+        name: "TypeScript",
+        backgroundColor: "#298EC6",
+        color: "#F9F4DA"
+    },
+    {
+        name: "Node.js",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA"
+    },
+    {
+        name: "Python",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA"
+    },{ 
+       name: "Ruby",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA"
+    },
+    {
+        name: "Assembly",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA" 
+    }
+]
